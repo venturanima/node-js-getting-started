6c365842ef0c90e13e69eae3081bbb9cbc49c3bc
Stephen Li <venturanima@gmail.com>
2021-12-24 17:46:15 -0500

    update with fetch

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const fetch = require('fetch')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 const pool = new Pool({
@@ -18,9 +19,27 @@ express()
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM stats');
+      const result = await client.query('SELECT * FROM stats;');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
+      console.log(`AAA results: ${result.rows[0].date}`);
+      console.log(`AAA results: ${result.rows[0].rank}`);
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/update', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const countFetch = await fetch('https://api.foldingathome.org/user-count');
+      const count = await countFetch.text();
+      const response = await fetch(`https://api.foldingathome.org/user/${user}`);
+      const userInfo = await response.json();
+      let d = new Date();
+      let currentDate = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`;
+      pool.query(`INSERT INTO stats(date,id,name,score,wus,rank)VALUES($1,$2,$3,$4,$5,$6)`, [currentDate, userInfo['id'],userInfo['name'],userInfo['score'],userInfo['wus'],`${userInfo['rank']} out of ${count}`], (err, res) => {});
       client.release();
     } catch (err) {
       console.error(err);
